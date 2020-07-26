8c9b2e2491b0d053088ccdbdc6b714b6a2bfa0b1
katoy <youichikato@gmail.com>
2020-07-27 07:10:16 +0900

    コメントを追加

--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
+// enviro:bit というボードをつないで, 温度、湿度、気圧、明るさを軽装して、シリアルポートに出力します。
+// 
+// makecode エディタ中の "コンソールを表示 デバイス" をクリックすると、シリアルポートへの出力をグラフ表示でみることがえきます。
+// 
+// USB ケーブルをつないでいる Mac や Raspberry PI 側では、つぎのようなコードで知り合うポートを読み取ることができます。
+// ----------------------
+// import serial   # pip3 install pyserial
+// 
+// # ls -l /dev/tty.*
+// ser = serial.Serial('/dev/tty.usbmodem14202', 115200)
+// microbitdata = ''
+// while True:
+//     line = str(ser.readline(), 'utf-8')
+//     print(line, end="")
+// ser.close()
+// -------------
+// (raspberry i の場合は '/dev/ttyACM0' とする
+// 
+// 参考情報
 basic.showLeds(`
     . . . . .
     . . . . .
